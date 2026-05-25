c6ea0094eff0abd29446d6e55220a3d65811975f
iki1uc <entertrainer@web.de>
2026-05-25 19:17:56 +0200

    Update c3.js

--- c3.js
+++ c3.js
@@ -1,1 +1,12 @@
+// c3.js – Cube‑3 Logik (QI‑Cube)
+
+function c3_out(ist, soll) {
+  document.getElementById("out").innerHTML = `
+    <div class="out-title">Wissenschaftlicher Output – Cube‑3</div>
+    <div class="out-ist"><b>IST:</b> ${ist}</div>
+    <div class="out-soll"><b>SOLL:</b> ${soll}</div>
+  `;
+}
+
+c3_out("QI aktiv", "Bereit für Verbund");
 
