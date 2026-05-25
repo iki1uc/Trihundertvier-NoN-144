3c5e51a5d13044e9b87f785b71366584c8a66151
iki1uc <entertrainer@web.de>
2026-05-25 19:18:10 +0200

    Update out3.js

--- out3.js
+++ out3.js
@@ -1,1 +1,18 @@
+// out3.js – Wissenschaftlicher Output für Cube‑3
+
+function out3_write(ist, soll, ableitung) {
+  document.getElementById("out").innerHTML = `
+    <div class="out-title">Wissenschaftlicher Output – Cube‑3</div>
+    <div class="out-ist"><b>IST:</b> ${ist}</div>
+    <div class="out-soll"><b>SOLL:</b> ${soll}</div>
+    <div class="out-ableitung"><b>Ableitung:</b> ${ableitung}</div>
+  `;
+}
+
+// Beispielstart
+out3_write(
+  "QI aktiv",
+  "Verbund bereit",
+  "Cube‑3 liefert Evolutions‑Impulse für den Verbund."
+);
 
