f9c284c6cd606c31dfb09ea16fa6fdf48954cf7e
iki1uc <entertrainer@web.de>
2026-05-27 22:10:24 +0200

    Update cube2.js

--- cube2.js
+++ cube2.js
@@ -1,1 +1,8 @@
+// cube2.js
+// CUBE‑2 = Hand / Oberfläche / 3×3 / RUN-Ebene
+
+export function cube2_run(anchor) {
+  console.log("CUBE‑2 ANKER:", anchor);
+  // hier später: 3×3‑Maske, RUN/LIST/EVAL, etc.
+}
 
